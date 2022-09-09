7dad79645d5e3e2aa6b87aaa00f271a6652d0165
Tomasz Tracz <t@tracz.me>
2022-09-09 10:07:35 +0100

    Fix style for one of the easter eggs

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,6 @@
-const addToTable = (value, table, classes = "") => {
+const addToTable = (value, table, style = "") => {
   if (value === 2) value = "2i";
+  if (value === 1337) style = "table-success";
   $(table).append(`
-              <li class="list-group-item ${classes}">${value}</li>`);
+              <li class="list-group-item ${style}">${value}</li>`);
 };
