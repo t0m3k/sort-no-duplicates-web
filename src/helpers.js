8cbfff74a93f7a2f67a0ccf78aa4a514ef9e891c
Tomasz Tracz <t@tracz.me>
2022-09-09 10:03:03 +0100

    Add 2i ester egg

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,5 @@
 const addToTable = (value, table, classes = "") => {
+  if (value === 2) value = "2i";
   $(table).append(`
               <li class="list-group-item ${classes}">${value}</li>`);
 };
